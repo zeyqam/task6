b2f1a8012e0e9c1855ed9fc6e543acf0376235f0
zeyqam <zegamda@code.edu.az>
2023-03-09 06:01:01 +0400

    employee

---
+++ book.cs
@@ -0,0 +1,49 @@
+﻿using ConsoleApp14.Models;
+using System.Threading.Channels;
+
+Book[] Books = new Book[2];
+for (int i = 0; i < Books.Length; i++)
+{
+    Console.WriteLine("Kitabin adi :");
+
+    string name = Console.ReadLine();
+
+    Console.WriteLine("Kitabin seife Sayi :");
+
+    int page = int.Parse(Console.ReadLine());
+    
+    while (!ChekName(name))
+    {
+        Console.WriteLine("adi duzgun daxil et");
+        name= Console.ReadLine();
+    }
+    while (!ChekCount(page))
+    {
+        Console.WriteLine("page duzgun daxil et");
+        page = int.Parse(Console.ReadLine());
+    }
+    
+
+    Book book = new Book(name, page);
+    Books[i] = book;
+}
+
+bool ChekName(string name)
+{
+    if(!string.IsNullOrWhiteSpace(name)&&name.Length>2)
+        return true;
+    return false;
+}
+
+    
+
+bool ChekCount(int pageCount)
+{
+    if(pageCount>10)
+        return true;
+    return false;
+}
+foreach (Book book in Books)
+{
+    Console.WriteLine($"Name :{book.Name}   pageCount:{book.PageCount}");
+}
---
+++ miniApp2.cs
@@ -0,0 +1,88 @@
+﻿using System; 
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mini.app_department.Models
+{
+    internal class Department
+    {
+        private string _name;
+        public string Name
+        {
+            get=>_name; set
+            {
+                if (!string.IsNullOrWhiteSpace(value.Trim(' ')))
+                {
+                    _name = value;
+                }
+                else
+                {
+                    Console.WriteLine("duzgun ad daxil et");
+                }
+            }
+        }
+
+        public string Surname { get; private set; }
+
+        public Employee[] Employees;
+        private string name;
+        private double salary;
+
+        public Department()
+        {
+            Employees = new Employee[0];
+        }
+        public void CreatEemployee()
+        {
+            
+            
+                Console.WriteLine("Employee Name: ");
+                string name = Console.ReadLine();
+                Console.WriteLine("Employee Surname: ");
+                string Surname = Console.ReadLine();
+
+                
+                
+                    Console.WriteLine("Employee Salary: ");
+                    double salary = double.Parse(Console.ReadLine());
+                
+            
+                
+            Employee employee = new Employee()
+            {
+                Name = name,
+                Surname = Surname,
+                Salary = salary
+            };
+            Array.Resize(ref Employees,Employees.Length + 1);
+            Employees[Employees.Length - 1] = employee;
+        }
+
+       
+
+        public void ShowEmployee()
+        {
+            foreach (Employee item in Employees)
+            {
+                Console.WriteLine($"name: {item.Name},surname:{item.Surname},salary:{item.Salary}");
+            }
+        }
+         
+            bool CheckSalary(int salary)
+            {
+                if (salary > 250)
+                    return true; else return false;
+            }
+    }
+ 
+}
+       
+              
+        
+
+
+    
+
+
